1455793eeb0f1887384054ca922b982bcf1ebc44
lina98st <156576799+lina98st@users.noreply.github.com>
2026-01-05 19:34:50 +0100

    Add Header component and basic placeholders for all components

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header className="header">
+            <h1>Typing Speed Test</h1>
+            {/* Personal best will go here later */}
+        </header>
+    )
+}
+
+export default Header 
